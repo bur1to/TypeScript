80cc5fa541d29f8b89cea64469b920c080565259
bur1t_o <daniil.dedenok@gmail.com>
2022-12-30 07:35:53 +0200

    change import

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,8 +1,10 @@
-import user = require('./users');
-import comment = require('./comments');
+import { getUsers, getUser, createUser, updateUser, deleteUser } from './users';
+import { getComments, getComment, createComment, updateComment, deleteComment } from './comments';
+
+const user = { getUsers, getUser, createUser, updateUser, deleteUser };
+const comment = { getComments, getComment, createComment, updateComment, deleteComment };
 
 export {
     user,
     comment,
-    
 };
